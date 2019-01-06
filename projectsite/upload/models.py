8db6cc124ecbabaa58c3e72e397cb473d973de78
camelcars <zcabzyc@ucl.ac.uk>
2019-01-06 16:54:25 +0000

    added some fields to models.py, modified the settings to make it work for my own database

--- projectsite/upload/models.py
+++ projectsite/upload/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
+
 class Img(models.Model):
-    img_url = models.ImageField(upload_to='foram')
+    imgLocation = models.CharField(max_length=100, default='None')
+    species = models.CharField(max_length=30, default='None')
+    parentImage = models.CharField(max_length=100, default='None')
