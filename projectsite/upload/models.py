b813b243b27f290df1035d980c3cc48e977af57f
Dearj611 <Dearj611@users.noreply.github.com>
2019-01-11 23:24:29 +0000

    added some ui

--- projectsite/upload/models.py
+++ projectsite/upload/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-
 class Img(models.Model):
     img_url = models.ImageField(upload_to='segment')
+    imgLocation = models.CharField(max_length=100, default='None')
     species = models.CharField(max_length=30, default='None')
     parentImage = models.CharField(max_length=100, default='None')
