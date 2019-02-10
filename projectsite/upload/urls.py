3dc83ef7d58ae07790def79074c31985cf53693f
Dearj611 <Dearj611@users.noreply.github.com>
2019-02-10 18:48:07 +0000

    added cleardatabase function for easy testing and edit upload view &ui

--- projectsite/upload/urls.py
+++ projectsite/upload/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from upload.views import BasicUploadView, showImg
+from upload.views import BasicUploadView, showImg, clear_database
 
 urlpatterns = [
+    path('clear/', clear_database, name='clear_database'),
     path('', BasicUploadView.as_view(), name='uploadImage'),
     path('show/', showImg, name='displayImage'),
 ]
--- projectsite/upload/views.py
+++ projectsite/upload/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from upload.models import Img
 from upload.forms import ImageUploadForm
 from django.http import JsonResponse
@@ -12,19 +12,28 @@ from pathlib import Path
 class BasicUploadView(View):
     def get(self, request):
         photos_list = Img.objects.all()
-        #forams = seg.get_all_forams(request.FILES.get('img'))
         #photos_list = Img.objects.filter(parentImage=photo.imgLocation.name)
         return render(self.request, 'upload/imgupload.html', {'photos': photos_list})
 
     def post(self, request):
-        form = ImageUploadForm(self.request.POST, self.request.FILES)
-        if form.is_valid():
-            photo = form.save()
-            _, file_ext = os.path.splitext(photo.imgLocation.name)
-            data = {'is_valid': True, 'name': photo.imgLocation.name, 'url': photo.imgLocation.url}
-        else:
-            data = {'is_valid': False}
-        return JsonResponse(data)
+        if request.method == 'POST':
+            form = ImageUploadForm(self.request.POST, self.request.FILES)
+            if form.is_valid():
+                photo = form.save()
+                #_, file_ext = os.path.splitext(photo.imgLocation.name)
+                #img = Img(imgLocation=request.FILES.get('img'))
+                #forams = seg.get_all_forams(request.FILES.get('img'))
+                #store_to_db(img, forams, 'ruber', './media/', '.jpg')
+                #show_forams = Img.objects.filter(parentImage=img)
+                #img_location = []
+                #for foram in show_forams:
+                #    img_location.append(foram.imgLocation)
+                data = {'is_valid': True, 'name': photo.imgLocation.name, 'url': photo.imgLocation.url}
+                #data = {'is_valid': True, 'content':img_location}
+                return JsonResponse(data)
+            else:
+                form = ImageUploadForm()
+                return render(request, 'name.html', {'form': form})
 
 
 def showImg(request):
@@ -33,3 +42,10 @@ def showImg(request):
         'imgs' : imgs
     }
     return render(request, 'upload/showImg.html', context)
+
+
+def clear_database(request):
+    for photo in Img.objects.all():
+        photo.imgLocation.delete()
+        photo.delete()
+    return redirect(request.POST.get('next'))
