5ecbd1841834f37194b779f63614028a604da73f
Dearj611 <Dearj611@users.noreply.github.com>
2019-02-18 12:20:10 +0000

    edit upload real-time display

--- projectsite/static/js/basic-upload.js
+++ projectsite/static/js/basic-upload.js
@@ -8,8 +8,13 @@ $(function () {
     dataType: 'json',
     done: function (e, data) {
       if (data.result.is_valid) {
-        $("#gallery tbody").prepend(
+        $("#gallery tbody").append(
           "<tr><td><img src='" + data.result.url + "'><p>"+ data.result.species + "</p></td></tr>"
+        )
+
+        $("#display").append(
+          "<div class='display_items'><img src='"+ data.result.url + "'style='width:100%'><p>"
+          + data.result.species +"</p><button> Change Tag</button></div>"
         )
       }
     }
