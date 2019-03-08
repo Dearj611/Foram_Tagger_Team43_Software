c8e775bf377ee58feaade7770fe0579fdb8361f6
camelcars <zcabzyc@ucl.ac.uk>
2019-03-08 15:44:17 +0000

    changed allowed_hosts

--- projectsite/projectsite/production_settings.py
+++ projectsite/projectsite/production_settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Tells your WSGI application what settings file to use
 os.environ['DJANGO_SETTINGS_MODULE'] = 'projectsite.production_settings'
