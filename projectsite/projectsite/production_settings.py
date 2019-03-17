ba9a6f95241936c37dc5a4f58b6e6a829d30e634
camelcars <zcabzyc@ucl.ac.uk>
2019-03-17 16:38:03 +0000

    modified production settings

--- projectsite/projectsite/production_settings.py
+++ projectsite/projectsite/production_settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,8 +135,8 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_ROOT = "/home/site/wwwroot/allstaticfiles/"
+STATIC_URL = STATIC_ROOT
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
