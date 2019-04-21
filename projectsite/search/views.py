fe3e3ea7e30b3d278434e35e17b062bdd31578d5
camelcars <zcabzyc@ucl.ac.uk>
2019-04-21 12:54:47 +0100

    fixed bug in pagination

--- projectsite/search/views.py
+++ projectsite/search/views.py
@@ -6,17 +6,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def search(request):
     if 'search' in request.GET:
-        imgs = []
         search_key = request.GET.get('search')
-        result = Img.objects.filter(species=str(search_key))
-        if len(result) == 0:
+        foram_list = Img.objects.filter(species=str(search_key))
+        if len(foram_list) == 0:
             message = "Sorry, there is no image matching this foram."
             return render(request,"search/searchImg.html",{"message":message})
-        for foram in result:
-            imgs.append(foram)
     else:
         foram_list = Img.objects.all()
-        page = request.GET.get('page', 1)
-        paginator = Paginator(foram_list, 20)
-        forams = paginator.page(page)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(foram_list, 20)
+    forams = paginator.page(page)
     return render(request,"search/searchImg.html",{"forams": forams})
